e016e88f6bf554ce2643f323248d3b9705628581
Ross Nomann <rossnomann@protonmail.com>
2019-06-01 21:20:11 +0300

    Use envy for config (closes #1)

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,24 @@
 use carapax::core::{types::Integer, Config as ApiConfig};
-use failure::{Error, Fail};
-use std::{
-    env::{var, VarError},
-    ffi::OsString,
-    net::SocketAddr,
-    num::ParseIntError,
-    str::FromStr,
-};
+use failure::Error;
+use serde::Deserialize;
+use std::net::SocketAddr;
+
+#[derive(Debug, Deserialize)]
+struct RawConfig {
+    token: String,
+    proxy: Option<String>,
+    webhook_address: Option<String>,
+    #[serde(default = "default_webhook_path")]
+    webhook_path: String,
+    redis_url: String,
+    chat_id: Integer,
+    shippering_pair_timeout: u64,
+    shippering_message_timeout: u64,
+}
+
+fn default_webhook_path() -> String {
+    String::from("/")
+}
 
 #[derive(Clone, Debug)]
 pub struct Config {
@@ -21,24 +33,19 @@ pub struct Config {
 
 impl Config {
     pub fn from_env() -> Result<Self, Error> {
-        let webhook_url = match get_var_string_opt("RUSTJERKBOT_WEBHOOK_ADDRESS")? {
-            Some(addr) => {
-                let addr = addr.parse::<SocketAddr>()?;
-                let path = get_var_string_opt("RUSTJERKBOT_WEBHOOK_PATH")?
-                    .unwrap_or_else(|| String::from("/"));
-                Some((addr, path))
-            }
+        let raw: RawConfig = envy::prefixed("RUSTJERKBOT_").from_env()?;
+        let webhook_url = match raw.webhook_address {
+            Some(addr) => Some((addr.parse::<SocketAddr>()?, raw.webhook_path)),
             None => None,
         };
-
         Ok(Config {
-            token: get_var_string("RUSTJERKBOT_TOKEN")?,
-            proxy: get_var_string_opt("RUSTJERKBOT_PROXY")?,
+            token: raw.token,
+            proxy: raw.proxy,
             webhook_url,
-            redis_url: get_var_string("RUSTJERKBOT_REDIS_URL")?,
-            chat_id: get_var_number("RUSTJERKBOT_CHAT_ID")?,
-            shippering_pair_timeout: get_var_number("RUSTJERKBOT_SHIPPERING_PAIR_TIMEOUT")?,
-            shippering_message_timeout: get_var_number("RUSTJERKBOT_SHIPPERING_MESSAGE_TIMEOUT")?,
+            redis_url: raw.redis_url,
+            chat_id: raw.chat_id,
+            shippering_pair_timeout: raw.shippering_pair_timeout,
+            shippering_message_timeout: raw.shippering_message_timeout,
         })
     }
 
@@ -49,50 +56,4 @@ impl Config {
         }
         config
     }
-}
-
-#[derive(Debug, Fail)]
-enum ConfigError {
-    #[fail(display = "'{}' does not contain an unicode string: {:?}", _0, _1)]
-    BadData(String, OsString),
-    #[fail(display = "'{}' is not specified", _0)]
-    Missing(String),
-    #[fail(display = "'{}' does not contain a number: {}", _0, _1)]
-    NotNumber(String, ParseIntError),
-}
-
-fn get_var_string<S>(name: S) -> Result<String, ConfigError>
-where
-    S: Into<String>,
-{
-    let name = name.into();
-    var(&name).map_err(|e| match e {
-        VarError::NotPresent => ConfigError::Missing(name),
-        VarError::NotUnicode(data) => ConfigError::BadData(name, data),
-    })
-}
-
-fn get_var_string_opt<S>(name: S) -> Result<Option<String>, ConfigError>
-where
-    S: Into<String>,
-{
-    let name = name.into();
-    match var(&name) {
-        Ok(value) => Ok(Some(value)),
-        Err(VarError::NotPresent) => Ok(None),
-        Err(VarError::NotUnicode(data)) => Err(ConfigError::BadData(name, data)),
-    }
-}
-
-fn get_var_number<S, N>(name: S) -> Result<N, ConfigError>
-where
-    S: Into<String>,
-    N: FromStr<Err = ParseIntError>,
-{
-    let name = name.into();
-    get_var_string(name.clone()).and_then(|value| {
-        value
-            .parse::<N>()
-            .map_err(|e| ConfigError::NotNumber(name, e))
-    })
 }
